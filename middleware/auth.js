0970ce7057f453f2629975d1c4f6b8be43ec0a73
Rlss91 <rlss91@gmail.com>
2022-06-29 10:51:57 +0300

    spasing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,19 @@
 const jwt = require("jsonwebtoken");
 
+module.exports = (req, res, next) => {
+  const token = req.query.token || req.header("x-auth-token");
 
-module.exports = (req,res,next) => {
-    const token = req.query.token || req.header('x-auth-token')
+  if (!token)
+    return res.status(401).json({ message: "Access denied no token provided" });
 
-    if(!token)
-    return res.status(401).json({message: 'Access denied no token provided'});
-
-    try{
-        const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = payload
-        next()
-    }
-    catch(e){
-         return res.status(401).json({
-             message: 'invalid token',
-             e: e
-         });
-
-    }
-}
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch (e) {
+    return res.status(401).json({
+      message: "invalid token",
+      e: e,
+    });
+  }
+};
