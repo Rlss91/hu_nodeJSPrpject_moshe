8e9b2b6ce7571c6dfad5fabb431bcdc3db3d57c8
Rlss91 <rlss91@gmail.com>
2022-07-18 14:24:47 +0300

    nodejs + react

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 const usersRouter = require("./routes/usersR");
 const connect = require("./utils/connect-db");
 const authRouter = require("./routes/auth");
@@ -13,9 +14,9 @@ app.use(express.urlencoded({ extended: true })); //parse body that contain form 
 app.use(express.json()); //parse body that contain json
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.end("its work");
-});
+// app.get("/", (req, res) => {
+//   res.end("its work");
+// });
 
 connect()
   .then(() => {
@@ -31,6 +32,12 @@ app.use("/api/cards", cardsRouter);
 
 app.get("/myapi", (req, res) => {
   res.json({ msg: "ok" });
+});
+
+/* react */
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 console.log(process.env.EXPRESS_PORT);
